7b0614e3cb14fecd80f12836705c30130ee70559
nsteiner <123197071+nsteiner@users.noreply.github.com>
2025-01-19 16:58:37 -0500

    Update test_app.py

--- backend/test_app.py
+++ backend/test_app.py
@@ -3,7 +3,8 @@ import sqlite3
 import os 
 
 # object to create a flask application
-app = Flask(__name__, template_folder=os.path.abspath('../frontend/templates'))
+app = Flask(__name__, template_folder=os.path.abspath('../frontend/templates'), 
+            static_folder=os.path.abspath('../frontend/static'))
 
 # setup database
 conn = sqlite3.connect("database_files/pf_app.db")
