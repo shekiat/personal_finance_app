a83e6b649a9047a1147a787874a8089696fdf4c6
nsteiner <nsteiner@oakland.edu>
2025-01-20 12:05:24 -0500

    commit

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 import sqlite3
+import os
 
 # object to create a flask application
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.abspath('../frontend/templates'), 
+            static_folder=os.path.abspath('../frontend/static'))
 
 # setup database
 conn = sqlite3.connect("database_files/pf_app.db")
@@ -12,4 +14,4 @@ cursor = conn.cursor() # set up cursor object for executing SQL statements
 @app.route('/')
 
 def index():
-    return 'hello yes'
+    return 'hello'
