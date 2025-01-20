71b6e14eed67f9306f181ecc4af2f458e5538944
nsteiner <nsteiner@oakland.edu>
2025-01-20 13:43:12 -0500

    Updated code to push and pull from db

--- backend/test_app.py
+++ backend/test_app.py
@@ -7,11 +7,10 @@ app = Flask(__name__, template_folder=os.path.abspath('../frontend/templates'),
             static_folder=os.path.abspath('../frontend/static'))
 
 # setup database
-conn = sqlite3.connect("database_files/pf_app.db")
+conn = sqlite3.connect("../backend/database_files/pf_app.db")
 cursor = conn.cursor() # set up cursor object for executing SQL statements
 
 # route to a web address (placeholder route /)
 @app.route('/')
-
 def index():
-    return 'hello'
+    return 'hello yes'
