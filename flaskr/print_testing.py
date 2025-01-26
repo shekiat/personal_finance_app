6b62fb1b3b08ccf2c25df812a9353109e8a25a99
nsteiner <nsteiner@oakland.edu>
2025-01-26 12:23:16 -0500

    Added db table and logic for getting totals per month

--- flaskr/print_testing.py
+++ flaskr/print_testing.py
@@ -1,9 +1,14 @@
 import sqlite3
+from db import *
 
 # setup database
 conn = sqlite3.connect("./database_files/pf_app.db")
 cursor = conn.cursor() # set up cursor object for executing SQL statements
 
-res = cursor.execute("SELECT * FROM TRANSACTIONS")
-trans_list = res.fetchall()
-print(trans_list)
+res = cursor.execute("SELECT TOTAL_BALANCE, TOTAL_EXPENSES, TOTAL_INCOME FROM TOTALS_PER_MONTH WHERE MONTH = ? AND YEAR = ?", (1, 2))
+
+total_balance = 0
+total_expenses = 0
+total_income = 0
+
+print(res.fetchall())
