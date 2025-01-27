365150fab3a8aa220980c4b875b44d595b0ec8d8
nsteiner <nsteiner@oakland.edu>
2025-01-27 13:28:54 -0500

    integrated changes from fe and be

--- flaskr/run_website.py
+++ flaskr/run_website.py
@@ -17,33 +17,26 @@ def index():
     # if called through "/submit", get whether the submit was successful; if called through "/month-change", get the month and year
     session_vars = {
         # for transactions
-        "submit_successful": session.get("submit_successful", True),
+        "submit_successful": session.get("submit_successful", None),
         "session_amount": session.get("amount", None),
         "session_date": session.get("date", None),
         "session_category": session.get("category", None),
         "session_memo": session.get("memo", ''),
 
         # for totals
-        "chosen_month": session.get("chosen_month", None),
-        "chosen_year": session.get("chosen_year", None)
+        "current_month": session.get("chosen_month", datetime.datetime.now().month),
+        "current_year": session.get("chosen_year", datetime.datetime.now().year)
     }
     session.clear()
 
     # get the totals and transactions for current month
-    current_month = datetime.datetime.now().month
-    current_year = datetime.datetime.now().year
     trans_list = []
 
-    if session_vars["chosen_month"] is not None:
-        parsed_chosen_date = parse_date(session_vars["session_date"])
-        chosen_month = parsed_chosen_date.month()
-        chosen_year = parsed_chosen_date.year()
+    current_month = session_vars['current_month']
+    current_year = session_vars['current_year']
 
-        total_values, total_diffs, total_diff_percs = check_and_read_month_totals(chosen_month, chosen_year) # [balance, expenses, income]
-        trans_list = read_transactions(chosen_month, chosen_year)
-    else:
-        total_values, total_diffs, total_diff_percs = check_and_read_month_totals(current_month, current_year)
-        trans_list = read_transactions(current_month, current_year)
+    total_values, total_diffs, total_diff_percs = check_and_read_month_totals(current_month, current_year) # [balance, expenses, income]
+    trans_list = read_transactions(current_month, current_year)
     
     # format differences for presentation
     for i in range(3):
@@ -51,8 +44,11 @@ def index():
             total_diffs[i] = "- $" + str(total_diffs[i])[1:]
         elif total_diffs[i] > 0:
             total_diffs[i] = "$" + str(total_diffs[i])
+
+    # get categories for drop down
+    category_list = read_categories()
         
-    return render_template("index.html", trans_list=trans_list, session_vars=session_vars, total_values=total_values, total_diffs=total_diffs, total_diff_percs=total_diff_percs)
+    return render_template("index.html", trans_list=trans_list, session_vars=session_vars, total_values=total_values, total_diffs=total_diffs, total_diff_percs=total_diff_percs, category_list=category_list)
     
     
 @bp.route('/submit', methods=['POST'])
@@ -75,6 +71,7 @@ def submit():
     if str(datetime.datetime.strptime(str(parsed_date), '%Y-%m-%d')) <= datetime.datetime.now(timezone.utc).strftime('%Y-%m-%d'):
         write_transaction(user="Jim Gorden", amount=amount, category=category, date=parsed_date, memo=memo)
         update_totals(parsed_date_full.month, parsed_date_full.year, amount)
+        session['submit_successful'] = True
     else:
         # if the date is in the future, notify user, add info to session so it stays in the input boxes
         session['submit_successful'] = False
@@ -84,4 +81,17 @@ def submit():
         if memo:
             session['memo'] = memo
     
+    return redirect(url_for("run_website.index"))
+
+
+@bp.route('/submit-date', methods=['POST'])
+def month_change():
+    month = request.form["month"]
+    year = request.form["year"]
+
+    month_number = datetime.datetime.strptime(month, "%B").month
+
+    session['chosen_month'] = month_number
+    session['chosen_year'] = year
+
     return redirect(url_for("run_website.index"))
