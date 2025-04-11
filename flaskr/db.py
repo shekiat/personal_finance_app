267c78c7c8e55ab30a356ebc31f45ac1b30e3b97
nsteiner <nsteiner@oakland.edu>
2025-04-11 13:23:00 -0400

    heroku bug test

--- flaskr/db.py
+++ flaskr/db.py
@@ -347,7 +347,7 @@ def write_income(user, amount, date, memo, user_id):
     if str(amount)[0] == '$':
         amount = int(str(amount)[1:])
 
-    data = (new_id, amount, date, memo if memo else '', user_id)
+    data = (new_id, amount, date, memo if memo else '', user_id[0])
     db_cursor.execute(f"INSERT INTO INCOME (INCOME_ID, INCOME_AMOUNT, INCOME_DATE, INCOME_MEMO, USER_ID) VALUES(%s, %s, %s, %s, %s)", data)
     db.commit()
     db_cursor.close()
