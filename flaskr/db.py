26f19ea132aa42de10ff94e4c363b5fd41a05244
nsteiner <nsteiner@oakland.edu>
2025-04-20 22:04:47 -0400

    print testing

--- flaskr/db.py
+++ flaskr/db.py
@@ -458,7 +458,7 @@ def read_income(month, year, id, user_group=0):
         income_list_element.append(full_name)
         income_list.append(income_list_element)
 
-    print(f"final testing income list: {income_list}")
+    print(f"final testing incomeincome_list)")
 
     return income_list
 
